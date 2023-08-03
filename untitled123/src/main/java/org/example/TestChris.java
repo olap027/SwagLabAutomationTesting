132d8362ebc6b73fed40a56b7132ab1e7ee679e4
olala <olap27@gmail.com>
2023-08-03 18:32:48 +0800

    added new sayHi method

--- untitled123/src/main/java/org/example/TestChris.java
+++ untitled123/src/main/java/org/example/TestChris.java
@@ -1,7 +1,10 @@
 package org.example;
 
 public class TestChris {
+    public static String sayHi() {
+        return "hello im chris";
+    }
     public static void main(String[] args) {
-        System.out.println("hello im chris");
+        sayHi();
     }
 }
